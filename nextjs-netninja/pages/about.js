bb6eee9aceadcdab07fee657380e1fcdf401e5b2
movalle4 <movalle4@uc.cl>
2021-07-21 13:58:22 -0400

    complete netninja nextjs tutorial

---
+++ nextjs-netninja/pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>lorem ipsum</p>
+      	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem libero, pellentesque et libero vel, pharetra mattis magna. Vivamus rutrum nisi ac ipsum fermentum, eget faucibus diam suscipit. Cras faucibus ullamcorper elit, non hendrerit elit vestibulum nec. Nulla eget vulputate arcu, placerat pulvinar mi. In at interdum sem. Maecenas at egestas ligula. Cras nec consequat lorem, a luctus quam. Proin eget sagittis lectus. Pellentesque semper ut massa sed sagittis.</p>
+      	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem libero, pellentesque et libero vel, pharetra mattis magna. Vivamus rutrum nisi ac ipsum fermentum, eget faucibus diam suscipit. Cras faucibus ullamcorper elit, non hendrerit elit vestibulum nec. Nulla eget vulputate arcu, placerat pulvinar mi. In at interdum sem. Maecenas at egestas ligula. Cras nec consequat lorem, a luctus quam. Proin eget sagittis lectus. Pellentesque semper ut massa sed sagittis.</p>
+		</div>
+	);
+}
+
+export default About;
---
+++ nextjs-netninja/pages/people/index.js
@@ -0,0 +1,28 @@
+import styles from '../../styles/People.module.css'
+import Link from 'next/link'
+
+export const getStaticProps = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await res.json();
+
+	return {
+		props: { people: data }
+	}
+}
+
+const People = ({ people }) => {
+	return (
+		<div>
+			<h1>People</h1>
+			{people.map(person => (
+				<Link href={'/people/' + person.id} key={person.id}>
+					<a className={styles.single}>
+						<h3>{person.name}</h3>
+					</a>
+				</Link>
+			))}
+		</div>
+	)
+}
+
+export default People;
